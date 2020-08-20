aca97a22803ac238e66065c25bf1c6bf0200aed1
jacksoncy <wxw198812@gmail.com>
2020-08-20 23:22:19 +0800

    add uri

--- include/websocket/uri.hpp
+++ include/websocket/uri.hpp
@@ -12,8 +12,9 @@
                     authority               path
         ┌───────────────┴───────────────┐┌───┴────┐
   abc://username:password@example.com:123/path/data?key=value&key2=value2#fragid1
-  └┬┘   └───────┬───────┘ └────┬────┘ └┬┘           └─────────┬─────────┘ └──┬──┘
-scheme  user information     host     port                  query         fragment
+  └┬┘   └───────┬───────┘ └────┬────┘ └┬┘           └─────────┬─────────┘
+└──┬──┘ scheme  user information     host     port                  query
+fragment
 
 */
 namespace Uranus::WebSocket
